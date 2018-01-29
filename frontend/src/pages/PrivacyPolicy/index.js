82695ca32a04fb745e42e24b8d27bbf006a2d9f9
Ryan Ong <ryan.ong0808@outlook.com>
2018-01-29 01:55:22 -0200

    Update privacy policy text

--- frontend/src/pages/PrivacyPolicy/index.js
+++ frontend/src/pages/PrivacyPolicy/index.js
@@ -11,42 +11,226 @@ const PrivacyPolicy = () => (
     subscribe
   >
     <Section className="page-content">
-      <h5>Effective Date: Feb 18, 2017</h5>
       <p>
-        Your privacy is extremely important to Charibin. We have developed a Privacy Policy
-        that covers how we collect, use, disclose, transfer, and store your information
-        on our website. Please take a moment to familiarize yourself with our privacy
-        practices and if you have any questions please direct them to{' '}
-        <a href="mailto:privacy@charibin.com">privacy@charibin.com</a>
+        Charibin, LLC. ("Charibin", "us", or "we") is committed to respecting and protecting
+        the privacy of our Users. This Privacy Policy, which is part of our overall Terms of
+        Service, covers personal information collected about our Users, which includes users
+        of Charibin Service (as defined in our Terms of Service), visitors to our web Site
+        (www.charibin.com, or "the Site"), and Merchants to Charibin (who are subject to
+        additional Merchant Terms).
+        Please review this Privacy Policy to better understand our practices as they apply to
+        personal information. By using Charibin Service or accessing the Site, you consent to
+        the data practices described in this Privacy Policy. If you do not agree to this
+        Privacy Policy, or any of the terms in the Terms of Service (including the Merchant
+        Terms if you are a Merchant), you may not continue to use Charibin Service or visit
+        the Site.
       </p>
-      <h4>Disclosure to Third Parties</h4>
+      <h4>CHANGES TO OUR PRIVACY POLICY</h4>
       <p>
-        At times Charibin may make certain personal information available to strategic partners
-        that work with Charibin to provide products and services, or that help Charibin market to
-        customers. Charibin shares personal information with companies who provide services such
-        as information processing, shipping, fulfilling customer orders, delivering products
-        to you, managing and enhancing customer data, providing customer service, assessing
-        your interest in our products and services, and conducting customer research or
-        satisfaction surveys. These companies are obligated to protect your information and
-        may be located wherever Charibin operates. We will share your personal information with
-        third parties only in the ways that are described in this privacy statement. We do
-        not sell your personal information to third parties.
+        From time to time, we may modify this Privacy Policy in our sole discretion. If we
+        make any updates or changes to this Privacy Policy, all modifications will be posted
+        to this Site, and will be effective immediately upon our posting. Using Charibin, or
+        using the Site, after updates are posted and effective constitutes acceptance of any
+        changes.
       </p>
-      <h4>Others</h4>
+      <h4>WHAT PERSONAL INFORMATION DO WE COLLECT?</h4>
       <p>
-        It may be necessary − by law, legal process, litigation, and/or requests from public
-        and governmental authorities within or outside your country of residence − for Charibin
-        to disclose your personal information. We may also disclose information about you if
-        we determine that for purposes of national security, law enforcement, or other issues
-        of public importance, disclosure is necessary or appropriate.
+        We collect personal information of all Users of Charibin (including visitors to its
+        Site), which may include name, phone number, address, email address, and payment
+        information. We may require you to provide personal information to interact with or
+        register for the Site. We require you to register for the Site in order to use Charibin
+        Service or to act as a Merchant to Charibin. If you participate in our opinion polls,
+        surveys, contests, or sweepstakes, we may collect additional personal information about
+        you in connection with your response or entry.
       </p>
       <p>
-        We may also disclose information about you if we determine that disclosure is reasonably
-        necessary to enforce our user agreement or protect our operations or users. Additionally,
-        in the event of a reorganization, merger, or sale we may transfer any and all personal
-        information we collect to the relevant third party. You will be notified via email and/or
-        a prominent notice on our website, of any change in ownership, uses of your personal
-        information, and choices you may have regarding your personal information.
+        Additionally, you may choose to make other personal information available to us in
+        connection with the consignment of an item, when you submit a video or blog for
+        publication on the Site, email us, or invite a friend to join the Site.
+      </p>
+      <h4>USE OF PERSONAL INFORMATION WE COLLECT</h4>
+      <p>
+        We use the personal information we collect to provide you with information, products,
+        and services that you request and that we believe may be of interest to you. We use the
+        personal information that we collect to register users, process requests and orders
+        (including membership registration), communicate with you, provide you with other services,
+        offers, and updates, as well as to administer our Site and comply with applicable laws.
+      </p>
+      <h5>COMMUNICATING WITH YOU</h5>
+      <p>
+        We may communicate with you about topics we believe you may be interested in, including
+        through newsletters, alerts, and reminders about recurring event sales. From time to time
+        we may send you service-related emails, such as a welcome email when you register for the
+        Site.
+      </p>
+      <p>
+        If you provide us personal information when you communicate with us (including in person,
+          by phone, or via email) this information may be shared with a Customer Care representative,
+        employee, or expert that is most likely to be able to address your inquiry. This information
+        will be used only for the purpose of responding to your inquiry.
+      </p>
+      <h5>INVITATIONS</h5>
+      <p>
+        When you invite a friend to join our Site, we will automatically send your friend an email
+        invitation on your behalf. We will not use your friend’s email address for any other purpose
+        unless they join our Site or request further information or response from us. Similarly, when
+        you email a message using the Invite a Friend function, we do not store the email addresses
+        that are used to facilitate the email communication.
+      </p>
+      <h5>OPINION POLLS AND SURVEYS</h5>
+      <p>
+        We may share personal information we collect from your responses to opinion polls or surveys
+        with third parties. If we do so, we will provide you with notice and the opportunity to consent.
+      </p>
+      <h5>CONTESTS AND AUCTIONS</h5>
+      <p>
+        If you enter a contest or auction conducted by Charibin and/or one of our partners, we may
+        contact you in connection with that particular contest or sweepstakes, including in order to
+        update you regarding your status, administer the promotion, or let you know that a promotion
+        has ended.
+      </p>
+      <h5>ONLINE TRACKING</h5>
+      <p>
+        Like most websites, we, our service providers, and our third-party partners may collect
+        certain information about your interaction with our Site. This information may be stored
+        in log files. Information stored in log files may include Internet Protocol (IP) addresses,
+        browser type, internet service provider (ISP), referring/exit pages, platform type, date/time
+        stamp, and other information.
+      </p>
+      <p>
+        We collect this information through the use of cookies and other similar technologies. We use
+        this information to analyze demographic and other trends, administer the site, track aggregate
+        usage of the Site, and provide you with relevant advertising.
+      </p>
+      <p>
+        Because an industry-standard Do-Not-Track protocol is not yet established, the choices we offer
+        will continue to operate as described in this Privacy Policy independent of any Do-Not-Track
+        signals.
+      </p>
+      <p>
+        Your Privacy Choices, and right to control our use of your personal information, are described
+        below.
+      </p>
+      <h4>SHARING OF PERSONAL INFORMATION</h4>
+      <h5>THIRD PARTIES</h5>
+      <p>
+        We may provide contractors access to your personal information in the course of assisting in
+        operating our business and providing products or services to you. These contractors include
+        vendors and suppliers that provide us with technology, services and/or content for the
+        operation and maintenance of our Site or Service. Access to your personal information by
+        these contractors is limited to the information reasonably necessary for the contractor to
+        perform its limited function. We require our contractors to keep the personal information
+        they are provided confidential and to comply with the terms of this Privacy Policy.
+      </p>
+      <p>
+        We will not rent, sell or share your personal information with third parties without your
+        consent except to provide you with a product or service you request or as described in this
+        Privacy Policy.
+      </p>
+      <h5>LEGAL DISCLAIMER</h5>
+      <p>
+        We may disclose personal information pursuant to a request from law enforcement or pursuant
+        to other legal or regulatory process, or as otherwise required by law, or, in our sole
+        discretion, to protect our rights, property or interests, including to enforce this Privacy
+        Policy or our Terms of Service. In the event that we are legally compelled to disclose your
+        personal information to a third party, we will make reasonable efforts to notify you unless
+        doing so would violate the law or court order.
+      </p>
+      <p>
+        In our sole discretion, we may also disclose information we collect to a third party when we
+        believe it is reasonably necessary to investigate or prevent harm, fraud, abuse, or illegal
+        conduct.
+      </p>
+      <p>
+        Information collected from this site may also be transferred as part of a merger or other
+        combination, or as part of a restructuring, bankruptcy, transfer of assets, or to our successors
+        in interest.
+      </p>
+      <h4>YOUR PRIVACY CHOICES</h4>
+      <h5>UPDATES TO YOUR INFORMATION</h5>
+      <p>
+        At your request, we will make efforts to update, change or delete personal information
+        Charibin maintains about you. In order to enforce the terms of this Privacy Policy, resolve
+        disputes, or provide information to satisfy applicable governmental laws, regulations,
+        investigations or proceedings, we may need to retain your personal information in our files.
+      </p>
+      <h5>CHOICE/OPT-OUT</h5>
+      <p>
+        Users who no longer wish to receive our newsletters, alerts, or promotional communications
+        may opt-out of receiving these communications.
+      </p>
+      <h5>HOW TO EXERCISE YOUR CHOICES</h5>
+      <p>
+        You may notify us of your choices or preferences by contacting us as set forth in the
+        "How to Contact Us" section below.
+      </p>
+      <h4>ADDITIONAL INFORMATION</h4>
+      <h5>TRANSFERS OF PERSONAL INFORMATION</h5>
+      <p>
+        Personal information collected from this Site will be transferred to and processed in the
+        United States, and may be transferred and processed to other countries outside of your country
+        of residence. We may also transfer personal information to service providers or other third
+        parties with whom we have contractual agreements. Other countries where your personal
+        information may be processed may have different legal protections for personal information
+        than your country of origin, including different standards for access by law enforcement,
+        courts, or other governmental authorities. Do not use this Site if you do not agree to the
+        transfer and processing of your personal information in the United States or other countries
+        outside your country of residence, or by third parties with whom we have contractual agreements
+        that include requirements for privacy and security measures.
+      </p>
+      <h5>THIRD PARTY WEBSITES</h5>
+      <p>
+        This Site contains links to other websites provided by third parties. Please be aware that
+        we do not control these third party websites or any of the content contained on those websites.
+        Once you have left our Site, we cannot be responsible for the privacy practices of such other
+        websites. We encourage you to exercise caution and to read the privacy policy for the website
+        you visit. The inclusion of hyperlinks to third party websites on our site in no way constitutes
+        an endorsement by us of such websites' content, actions or policies.
+      </p>
+      <h5>AGGREGATED INFORMATION</h5>
+      <p>
+        We may share aggregated information about you with third parties. Aggregated information is
+        non-personal information about you that is combined with the non-personal information of other
+        users. We may not limit the third parties' use of the aggregated information.
+      </p>
+      <h5>YOUR CALIFORNIA PRIVACY RIGHTS</h5>
+      <p>
+        California's "Shine The Light" law permits California residents to annually request and obtain
+        information free of charge about what personal information is disclosed to third parties for
+        direct marketing purposes in the preceding calendar year. Charibin does not distribute your
+        personal information to outside parties for their direct marketing, except as provided for in
+        this Privacy Policy, without your consent.
+      </p>
+      <h5>ACCESS BY CHILDREN OR MINORS</h5>
+      <p>
+        Our Site is not directed to children under the age of 13, and our Terms of Service prohibit
+        minors from using our Site or Service. We do not knowingly collect any information from children
+        under 13.
+      </p>
+      <h5>SECURITY</h5>
+      <p>
+        We take precautions to protect information that we collect from our Users. We protect the
+        information entrusted to us through physical, technical and administrative safeguards. These
+        protections include restricting access to personal information to employees who need the
+        information to perform a specific job. Charibin provides its employees training on its security
+        and privacy practices.
+      </p>
+      <p>
+        However, no information security safeguards can be guaranteed to be completely secure, so you
+        should use caution whenever providing personal information online, electronically or otherwise.
+      </p>
+      <h5>HOW TO CONTACT US</h5>
+      <p>
+        If you have any questions or comments about this Privacy Notice, or if you would like us
+        to update information we have about you or your preferences, please contact us by email
+        at <a href="mailto:contact@charibin.com">contact@charibin.com</a>.
+      </p>
+      <p>
+        You also may write to us at:<br />
+        Charibin, LLC.<br />
+        Attn: Legal Department<br />
+        2000 Cabot Blvd. West, Suite 110<br />
+        Langhorne, PA 19047
       </p>
       <ContactTextLink />
     </Section>
